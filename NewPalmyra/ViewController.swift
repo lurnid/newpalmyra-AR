6e8d7414211227f3d75195a23d5bbbfa6baf801d
Mahmoud "Max" Wardeh <m@visiva.co>
2018-09-25 21:43:25 +0100

    Add project files

---
+++ NewPalmyra/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  NewPalmyra
+//
+//  Created by Mahmoud Wardeh on 21/09/2018.
+//  Copyright © 2018 Visiva Ltd. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var nodeModel:SCNNode!
+    let nodeName = "arch-of-triumph"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        //sceneView.debugOptions = ARSCNDebugOptions.showWorldOrigin
+        sceneView.antialiasingMode = .multisampling4X
+
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        let modelScene = SCNScene(named: "art.scnassets/arch-of-triumph.scn")!
+        
+        nodeModel = modelScene.rootNode.childNode(withName: nodeName, recursively: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView)
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        let hitResults: [SCNHitTestResult] =
+            sceneView.hitTest(location, options: hitTestOptions)
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                node.removeFromParentNode()
+                return
+            }
+        }
+        
+        let hitResultsFeaturePoints: [ARHitTestResult] =
+            sceneView.hitTest(location, types: .featurePoint)
+        if let hit = hitResultsFeaturePoints.first {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
+    }
+    
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+
+    // MARK: - ARSCNViewDelegate
+    
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+                
+                // Add model as a child to the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
